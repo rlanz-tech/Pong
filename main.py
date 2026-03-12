2245f87d5ea9eb67b1883fc943228c1c1de796da
SebastianH05 <hacker.sebasti-it25@it.dhbw-ravensburg.de>
2026-03-12 20:38:39 +0100

    Visualisierung des Balls

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ bar_width = 5
 bar_height = 1
 barstartpos = 350
 
+# Optische Wiedergabe des Spielfensters: Bars(Robin Lanz) und Ball(Sebastian Hacker)
 wn = logic.create_window()
 logic.create_bars(bar_width, bar_height, barstartpos)
-
+logic.create_ball()
 
 while True:
     wn.update()
