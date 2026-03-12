2bd94e7965ad1b7b30fc81500bec9ba23e7cc525
Robin <robin.lanz1@gmx.de>
2026-03-12 18:16:57 +0100

    Seitliche Schläger grafisch hinzugefügt

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 import logic
 
+# Standardwerte in Variablen
+bar_width = 5
+bar_height = 1
+barstartpos = 350
+
 wn = logic.create_window()
+#logic.create_bars(logic.bar_width, logic.bar_height, logic.barstartpos)
+
 
 while True:
     wn.update()
-    
-
+    logic.create_bars(bar_width, bar_height, barstartpos)
