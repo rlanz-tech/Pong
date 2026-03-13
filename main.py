811d28de8d4dcfb0cbee4027fe7c35bf89312aad
SebastianH05 <hacker.sebasti-it25@it.dhbw-ravensburg.de>
2026-03-13 15:17:35 +0100

    Ball Bewegung und Kommentaranpassung im main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import logic
 
 # Standardwerte in Variablen
+# Anmerkung: Im Vollbild stimmt die Posistion der Schläger nicht mehr
 bar_width = 5
 bar_height = 1
 barstartpos = 350
@@ -11,9 +12,12 @@ bar_speed = 20
 score_one = 0
 score_two = 0
 
-# Optische Wiedergabe des Spielfensters: Bars(Robin Lanz) und Ball(Sebastian Hacker)
+# Optische Wiedergabe des Spielfensters, Schläger (Robin Lanz) und Ball (Sebastian Hacker) und Scoreboard (Christina Kaiser) werden erstellt.
 wn = logic.create_window()
-bar_left, bar_right = logic.create_bars(bar_width, bar_height, barstartpos)     # Importieren der Objekte Schläger aus logic.py
+bar_left, bar_right = logic.create_bars(bar_width, bar_height, barstartpos) 
+ball = logic.create_ball()
+
+logic.create_scoreboard()
 
 # Tasteneingabe führt Bewegungsfunktion aus (Robin Lanz)
 wn.listen()
@@ -22,9 +26,11 @@ wn.onkeypress(lambda: logic.bar_left_down(bar_left, bar_speed), "s")
 wn.onkeypress(lambda: logic.bar_right_up(bar_right, bar_speed), "Up")
 wn.onkeypress(lambda: logic.bar_right_down(bar_right, bar_speed), "Down")
 
-logic.create_ball()
-logic.create_scoreboard()
-
 
 while True:
     wn.update()
+
+    # Ballbewegung (Sebastian Hacker)
+    logic.move_ball(ball)
+
+
